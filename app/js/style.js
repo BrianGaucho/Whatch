62b4448b69402ada6f83dfce11d1e7066afe0215
Brian Nyamweya <briangnyamweya@gmail.com>
2023-06-15 16:25:55 +0300

    -

--- app/js/style.js
+++ app/js/style.js
@@ -9,18 +9,15 @@ let ampm = document.getElementById('ampm');
 let h= new Date().getHours()
 let m= new Date().getMinutes()
 let s=new Date().getSeconds()
-// let pm=h >=12? "PM" : "AM";
 
 hours.innerHTML = h;
 minutes.innerHTML = m;
 seconds.innerHTML = s;
 
-// CONVERT
 if (h >12){
     h = h -12;
 }
 
-// add zero
 
 h = (h < 10)? "0" + h : h;
 m = (m < 10)? "0" + m : m;
